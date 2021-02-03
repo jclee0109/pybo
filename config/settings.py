c99302556f2a99bae662f86e44023fd6ed5181c6
jclee0109 <jclee0109@gist.ac.kr>
2021-02-03 16:14:16 +0900

    ALLOWED_HOSTS 변경

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-sw()a3-flfk&=$f4@xx_w*9^v(6nk0l3fkx9oc+&0-vnzm$$9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['15.165.24.147']
 
 
 # Application definition
